602bfb104e813f59e0a4ed71a5b515a87c8fa1a2
Wczy <chengzhi.wu@definesys.com>
2020-09-02 13:58:44 +0800

    伍城志ssm项目

--- src/main/java/com/wcz/tmall/comparator/ProductAllComparator.java
+++ src/main/java/com/wcz/tmall/comparator/ProductAllComparator.java
@@ -5,7 +5,7 @@ import com.wcz.tmall.pojo.Product;
 import java.util.Comparator;
 
  /**
-  * @Date: 2020-08-28 20:01
+  * @Date: 2020-08-28 20:28
   * @author: ChengZhi.Wu9299
   * @Description: 综合比较器，销量和评价按7：3的比列权重比较，高的放前面
   */
